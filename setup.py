8cee0dc09787b90c8eafdcbefc41899e847c9edc
Jivan Amara <Development@JivanAmara.net>
2016-07-22 13:46:46 -0700

    Bumped version to 0.2.0. Tagged as v0.2.0.

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open('README.rst', 'r') as rmf:
 
 setup(
     name="ttlib",
-    version="0.1.1",
+    version="0.2.0",
     author="Jivan Amara",
     author_email="Development@JivanAmara.net",
     packages=['ttlib', 'ttlib.characteristics', 'ttlib.normalization', 'ttlib.predictors'],
